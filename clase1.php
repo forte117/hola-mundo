4c46f2ef9e03133392e7947a74a8feee832a32bb
forte117 <131395897+forte117@users.noreply.github.com>
2023-04-21 10:22:33 -0300

    Create clase1.php

---
+++ clase1.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>IES Juan Mantovani</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <h1>Bienvenidos a la clase n°1</h1>
+  <h2>Materia: Practicas Profesionalizantes</2>
+</body>
+</html>
